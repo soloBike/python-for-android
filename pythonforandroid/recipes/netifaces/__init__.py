a11e8019520e0e40ecbb31afd8e9d43597afc318
Mathieu Virbel <mat@kivy.org>
2016-11-16 01:56:30 +0100

    netifaces: fixes compilation

--- pythonforandroid/recipes/netifaces/__init__.py
+++ pythonforandroid/recipes/netifaces/__init__.py
@@ -1,25 +1,21 @@
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 from os.path import join
 
-
 class NetifacesRecipe(CompiledComponentsPythonRecipe):
     name = 'netifaces'
     version = '0.10.4'
     url = 'https://pypi.python.org/packages/source/n/netifaces/netifaces-{version}.tar.gz'
     site_packages_name = 'netifaces'
-    depends = ['python2', 'setuptools']
+    depends = [('python2', 'python3crystax'), 'setuptools']
+    call_hostpython_via_targetpython = False
 
-    def get_recipe_env(self, arch=None):
+    def get_recipe_env(self, arch):
         env = super(NetifacesRecipe, self).get_recipe_env(arch)
-
-        # TODO: fix hardcoded path
-        # This is required to prevent issue with _io.so import.
-        hostpython = self.get_recipe('hostpython2', self.ctx)
-        env['PYTHONPATH'] = (
-            join(hostpython.get_build_dir(arch.arch), 'build',
-                 'lib.linux-x86_64-2.7') + ':' + env.get('PYTHONPATH', '')
-        )
+        env['CFLAGS'] += ' -I' + join(self.ctx.get_python_install_dir(), 'include/python2.7')
+        env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
+        env['LDFLAGS'] += ' -lpython2.7'
         return env
+
 
 
 recipe = NetifacesRecipe()
